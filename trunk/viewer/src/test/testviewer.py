75a166bfa2662de89f33f18889779989bfe8ae93
odorovic <odorovic@36354658-336f-11de-94d6-134fa40061eb>
2009-04-28 13:55:47 +0000

    Inicijalni sors import

---
+++ trunk/viewer/src/test/testviewer.py
@@ -0,0 +1,18 @@
+'''
+Created on Apr 24, 2009
+
+@author: odor
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
