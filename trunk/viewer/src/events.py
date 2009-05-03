baf8abc9cd792fe2ca0edba5b66a31ffcccc169b
odorovic <odorovic@36354658-336f-11de-94d6-134fa40061eb>
2009-05-03 00:09:41 +0000

    Dodata tri custom eventa. Za sada ne rade preterano mnogo

---
+++ trunk/viewer/src/events.py
@@ -0,0 +1,38 @@
+'''Dogadjaji specificni za aplikaciju
+Created on May 3, 2009
+
+@author: odor
+'''
+
+import wx
+
+class SchChangedEvent(wx.PyCommandEvent):
+    '''Dogadjaj promene selektovane sheme.'''
+    
+    type = wx.NewEventType()
+    binder = PyEventBinder(type, 1)
+    
+    def __init__(self, type, id):
+        wx.PyCommandEvent.__init__(self, type, id)
+        
+EVT_SCH_CHANGED = SchChangedEvent.binder 
+
+class SimStateEvent(wx.PyCommandEvent):
+    '''Dogadjaj promene stanja simulatora.'''
+    type = wx.NewEventType()
+    binder = PyEventBinder(type, 1)
+    
+    def __init__(self, type, id):
+        wx.PyCommandEvent.__init__(self, type, id)
+
+EVT_SIM_STATE = SimStateEvent.binder
+
+class SimCommandEvent(wx.PyCommandEvent):
+    '''Dogadjaj unete komande simulatoru.'''
+    type = wx.NewEventType()
+    binder = PyEventBinder(type, 1)
+    
+    def __init__(self, type, id):
+        wx.PyCommandEvent.__init__(self, type, id)
+
+EVT_SIM_CMD = SimCommandEvent.binder
