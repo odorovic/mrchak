0649f262ff60844a3004494ca29d99b8713ec638
odorovic <odorovic@36354658-336f-11de-94d6-134fa40061eb>
2009-05-03 00:45:42 +0000

    Malo refaktorisani dogadjaji radi lakseg rada. DOdata polka type i binder, umesot modul-globalnih promenljivih.

--- viewer/src/events.py
+++ viewer/src/events.py
@@ -10,29 +10,29 @@ class SchChangedEvent(wx.PyCommandEvent):
     '''Dogadjaj promene selektovane sheme.'''
     
     type = wx.NewEventType()
-    binder = PyEventBinder(type, 1)
+    binder = wx.PyEventBinder(type, 1)
     
-    def __init__(self, type, id):
-        wx.PyCommandEvent.__init__(self, type, id)
+    def __init__(self, id):
+        wx.PyCommandEvent.__init__(self, SchChangedEvent.type, id)
         
 EVT_SCH_CHANGED = SchChangedEvent.binder 
 
 class SimStateEvent(wx.PyCommandEvent):
     '''Dogadjaj promene stanja simulatora.'''
     type = wx.NewEventType()
-    binder = PyEventBinder(type, 1)
+    binder = wx.PyEventBinder(type, 1)
     
-    def __init__(self, type, id):
-        wx.PyCommandEvent.__init__(self, type, id)
+    def __init__(self, id):
+        wx.PyCommandEvent.__init__(self, SimStateEvent.type, id)
 
 EVT_SIM_STATE = SimStateEvent.binder
 
 class SimCommandEvent(wx.PyCommandEvent):
     '''Dogadjaj unete komande simulatoru.'''
     type = wx.NewEventType()
-    binder = PyEventBinder(type, 1)
+    binder = wx.PyEventBinder(type, 1)
     
-    def __init__(self, type, id):
-        wx.PyCommandEvent.__init__(self, type, id)
+    def __init__(self, id):
+        wx.PyCommandEvent.__init__(self, SimCommandEvent.type, id)
 
 EVT_SIM_CMD = SimCommandEvent.binder
